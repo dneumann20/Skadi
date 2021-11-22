c878cd1068434710bfb65e1a023f081289405ef6
Dennis Neumann <dennis2.neumann@student.uni-siegen.de>
2021-11-22 16:48:53 +0200

    WIP connect app to hono via MQTT

--- mobile/src/main/java/com/example/skadi/MainActivity.java
+++ mobile/src/main/java/com/example/skadi/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.skadi;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +15,13 @@ import com.google.android.gms.wearable.MessageClient;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Receives its own events using a listener API designed for foreground activities. Updates a data
@@ -28,22 +29,49 @@ import java.util.concurrent.Executors;
  * the paired wearable.
  */
 public class MainActivity extends AppCompatActivity implements
-        MessageClient.OnMessageReceivedListener,
-        View.OnClickListener {
+        MessageClient.OnMessageReceivedListener {
 
     String dataPath = "/message_path";
+    String TAG = "Mobile MainActivity";
 
-    Button startSensorButton;
+    Button btnToggleSensor;
+    Button btnToggleMqttConnection;
 
     TextView currentHeartRate;
     TextView heartRateValue;
     TextView recordingStatus;
+    TextView honoConnectionStatus;
 
-    protected Handler handler;
-    String TAG = "Mobile MainActivity";
+    // TODO remove after testing
+    TextView testMessageToHono;
+
     boolean dataCollectionActive = false;
+    boolean honoConnectionActive = false;
+
     private static final String msgStart = "START_RECORDING";
     private static final String msgStop = "STOP_RECORDING";
+
+    /**
+     * Hono related variables
+     */
+    //Instance of the helper class for the connection and message sending functions
+    PahoMqttClient pahoMqttClient = new PahoMqttClient();
+    MqttAndroidClient client;
+
+    /**
+     * IPs and IDs generated via setup.sh file from Smadis honoScripts
+     * Should be moved as environment variables for security reasons
+     */
+    String MQTT_ADAPTER_IP_URI = "tcp://20.103.90.35:1883";
+    String TENANT_ID = "13848e8d-cb99-469f-a686-747c6dace783";
+    String CLIENT_DEVICE_ID = "a33a0a20-6c10-4b80-a359-b88b5c70971e";
+    String USERNAME = CLIENT_DEVICE_ID +"@"+ TENANT_ID;
+    String PASSWORD = "j2lZb0qWYWFZAAe";
+
+    String topic =  "event"+"/"+TENANT_ID+"/"+CLIENT_DEVICE_ID+"/";
+
+    MemoryPersistence persistence = new MemoryPersistence();
+    int qos = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,21 +80,32 @@ public class MainActivity extends AppCompatActivity implements
 
         // Set text views
         currentHeartRate = findViewById(R.id.currentHeartRate); // Plain textView with static text
-        heartRateValue = findViewById(R.id.heartRateValue);
-        recordingStatus = findViewById(R.id.recordingStatus);
+        heartRateValue = findViewById(R.id.heartRateValue); // textView that shows the actual heart rate
+        recordingStatus = findViewById(R.id.recordingStatus); // Shows whether recording is on or off
+        honoConnectionStatus = findViewById(R.id.honoConnectionStatus); // Shows whether connected to Hono
 
-        // Set button
-        startSensorButton = findViewById(R.id.start_data_collection);
-        startSensorButton.setOnClickListener(this);
+        //TODO Remove
+        testMessageToHono = findViewById(R.id.testMessageToHono);
 
-        //message handler for the send thread.
-        handler = new Handler(new Handler.Callback() {
+        // Set buttons
+        btnToggleSensor = findViewById(R.id.start_data_collection);
+        btnToggleSensor.setOnClickListener(this::toggleDataCollection);
+
+        btnToggleMqttConnection = findViewById(R.id.toggle_hono_connection);
+        btnToggleMqttConnection.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean handleMessage(Message msg) {
-                Bundle stuff = msg.getData();
-                return true;
+            public void onClick(View v) {
+                try {
+                    toggleHonoConnection(v);
+                }
+                catch (MqttException | UnsupportedEncodingException e){
+                    Log.d(TAG, "Connect via Button Click "+e.toString());
+                    e.printStackTrace();
+                }
+
             }
         });
+
     }
 
     @Override
@@ -82,39 +121,102 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     /**
-     * This is a simple receiver add/removed in onResume/onPause
-     * It receives the message from the wear device and displays to the screen.
+     * Receives Messages from Polar watch and displays sensor value on screen
+     * TODO WIP: Sends the data to Hono
      */
     @Override
     public void onMessageReceived(@NonNull MessageEvent messageEvent) {
         Log.d(TAG, "onMessageReceived() A message from watch was received:"
                 + messageEvent.getRequestId() + " " + messageEvent.getPath());
         String message = new String(messageEvent.getData());
-        Log.v(TAG, "Main activity received message: " + message);
+        Log.d(TAG, "Main activity received message: " + message);
         Log.d(TAG, "Received data: " + message);
         // Display message in UI
         heartRateValue.setText(message);
+
+        //TODO forward via MQTT Client
+        /*try {
+            pahoMqttClient.publishMessage(client, message, qos, topic+"heartRate");
+
+            Log.i("MQTT", "Message published");
+        } catch (MqttException | MqttPersistenceException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+
     }
 
-    public void onClick(View v) {
+    public void toggleDataCollection(View v) {
         this.dataCollectionActive = !this.dataCollectionActive;
         if(this.dataCollectionActive) {
 
-            startSensorButton.setText(R.string.stop_data_collection);
+            heartRateValue.setText(R.string.plain_zero); // Initial value is 0
+            btnToggleSensor.setText(R.string.stop_data_collection);
             recordingStatus.setText(R.string.data_collection_on);
 
-            //Requires a new thread to avoid blocking the UI
+            // Sends message to start heart rate Sensor
             new SendThread(dataPath, msgStart).start();
         }
 
         else {
-            heartRateValue.setText(R.string.plain_zero);
-            startSensorButton.setText(R.string.start_data_collection);
-            recordingStatus.setText(R.string.data_collection_on);
+            heartRateValue.setText("");
+            btnToggleSensor.setText(R.string.start_data_collection);
+            recordingStatus.setText(R.string.data_collection_off);
 
+            // Sends message to stop heart rate Sensor
             new SendThread(dataPath, msgStop).start();
         }
+    }
 
+    private void toggleHonoConnection(View v) throws MqttException, UnsupportedEncodingException {
+        if(!this.honoConnectionActive) {
+            connectMQTTClient();
+        }
+        else {
+            disconnectMQTTClient(client);
+        }
+    }
+
+    /**
+     * Trying connect to Hono, make sure that the receiver script is running!
+     */
+    private void connectMQTTClient() {
+
+        try {
+            client = pahoMqttClient.getMqttClient(getApplicationContext(),
+                    MQTT_ADAPTER_IP_URI, CLIENT_DEVICE_ID, USERNAME, PASSWORD);
+            honoConnectionActive = !honoConnectionActive;
+            honoConnectionStatus.setText(R.string.disconnected_from_hono);
+            btnToggleMqttConnection.setText(R.string.connect_to_hono);
+        } catch (MqttException e) {
+            Log.d(TAG, "Failure to connect."+e.toString());
+        }
+
+        //Send test message
+        try {
+            String message = "connectSuccess";
+            pahoMqttClient.publishMessage(client, message, qos, topic+"sendTestMessage");
+
+            testMessageToHono.setText(R.string.hono_message_test);
+        } catch (MqttException | UnsupportedEncodingException e){
+            Log.d(TAG, "Failure to send: "+e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Trying disconnect from MQTT Server
+     */
+    private void disconnectMQTTClient(@NonNull MqttAndroidClient client) {
+        try {
+            pahoMqttClient.disconnect(client);
+            honoConnectionActive = !honoConnectionActive;
+            honoConnectionStatus.setText(R.string.disconnected_from_hono);
+            btnToggleMqttConnection.setText(R.string.connect_to_hono);
+            Log.d(TAG, "Successfully disconnected");
+        } catch (MqttException me) {
+            Log.d(TAG, "Failure to disconnect: " + me.toString());
+        }
     }
 
     // Thread to send the message to the wearable device. Thread is destroyed afterwards.
